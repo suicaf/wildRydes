faffeb7c79e1a07e2d04e276ab0adf526508a833
Jake <suicaf@live.com>
2025-02-16 15:14:33 -0800

    Connected to AWS Cognito User pool

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-1_wZc2j7GiC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3jbajibslhu94h2fudlvf2mobd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
